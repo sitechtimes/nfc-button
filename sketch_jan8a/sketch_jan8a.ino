357a920d90351acdc5ab5bbc974513ae852aeb18
Samuel Kipnis <sam.kipnis@gmail.com>
2025-01-14 11:00:32 -0500

    Update sketch_jan8a.ino

--- sketch_jan8a/sketch_jan8a.ino
+++ sketch_jan8a/sketch_jan8a.ino
@@ -8,15 +8,21 @@ const char* ssid = "DOEGuest";
 const char* password = "NYC$itevent";
 
 // MQTT broker details
+
 // const char* mqttServer = "70ae867d15a74e699cbf5e0c0306eda4.s1.eu.hivemq.cloud";
 // const int mqttPort = 8883;
 // const char* mqttUser = "admin";
 // const char* mqttPassword = "Adminpassword1";
 
-const char* mqttServer = "b37.mqtt.one";
-const int mqttPort = 8083;
-const char* mqttUser = "deltuy8445";
-const char* mqttPassword = "30dinoqrtx";
+// const char* mqttServer = "b37.mqtt.one";
+// const int mqttPort = 8083;
+// const char* mqttUser = "deltuy8445";
+// const char* mqttPassword = "30dinoqrtx";
+
+const char* mqttServer = "kef90981.ala.us-east-1.emqxsl.com";
+const int mqttPort = 8883;
+const char* mqttUser = "admin";
+const char* mqttPassword = "adminpassword";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -26,34 +32,39 @@ void setup() {
 
   bool connected = connectWiFi();
   if(!connected) {
-    Serial.println('WiFi not connected')
+    Serial.println('WiFi not connected');
   } else {
-    Serial.println('WiFi connected')
+    Serial.println('WiFi connected');
   }
 
-
   // Configure MQTT client
-  // client.setServer(mqttServer, mqttPort);
-  // client.setCallback(callback); // Set callback called when receiving a message
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback); // Set callback called when receiving a message
 }
 
 bool connectWiFi() {
   Serial.print("Connecting to Wifi...");
   WiFi.begin(ssid, password);
-  const int attempts = 0;
+  int attempts = 0;
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
     attempts++;
 
-    if(attempts = 10) {
+    if(attempts >= 10) {
       return false;
     }
   }
   Serial.println("\nConnected to Wifi");
   Serial.println(WiFi.localIP());
   return true;
+}
+
+bool configMQTTClient() {
+  // Configure MQTT client
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback); // Set callback called when receiving a message
 }
 
 void loop() {
@@ -75,8 +86,6 @@ void reconnect() {
     Serial.print("Connecting to MQTT broker...");
     if (client.connect("ESP32Client",  mqttUser, mqttPassword)) {
       Serial.println("connected");
-
-      // Subscribe to a topic
       client.subscribe("buttonPresses");
     } else {
       Serial.print("failed, rc=");
